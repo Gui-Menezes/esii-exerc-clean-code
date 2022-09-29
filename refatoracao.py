be9e58d783563c96ce71f3da6bf847a831eb1402
Guilherme <guibenmen@gmail.com>
2022-09-29 18:36:26 -0300

    Primeiro commit - inclusão da função que verificar idade

---
+++ refatoracao.py
@@ -0,0 +1,16 @@
+def minhaFuncao(idade):
+    if idade < 14 :
+        resultado =  "Criança"
+    elif idade < 18 :
+        resultado = "Adolecente"
+    elif idade < 65:
+        resultado = "Adulto"
+    else :
+        resultado = "Idoso"
+
+    return resultado
+
+# programa principal
+seuNome  = input("Digite seu nome: ")
+suaIdade = int(input("Digite sua idade: ") )
+print ( seuNome,"você é" , minhaFuncao(suaIdade) )
