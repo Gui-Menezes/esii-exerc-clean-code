1558ef8ed7aa5471cf548cf8b7edb26be095ec95
Guilherme <guibenmen@gmail.com>
2022-09-29 18:44:25 -0300

    Segundo commit - aplicação de clean code na função existente

--- refatoracao.py
+++ refatoracao.py
@@ -1,16 +1,20 @@
-def minhaFuncao(idade):
-    if idade < 14 :
+def definicaoDeFasesDaVida(idade):
+    if idade < 14:
         resultado =  "Criança"
-    elif idade < 18 :
+    elif idade < 18:
         resultado = "Adolecente"
     elif idade < 65:
         resultado = "Adulto"
-    else :
+    else:
         resultado = "Idoso"
 
     return resultado
 
+def imprimirFaseDaVida(valor):
+    print(f"{seuNome}, você é {valor}")
+
 # programa principal
 seuNome  = input("Digite seu nome: ")
 suaIdade = int(input("Digite sua idade: ") )
-print ( seuNome,"você é" , minhaFuncao(suaIdade) )
+
+imprimirFaseDaVida(definicaoDeFasesDaVida(suaIdade))
